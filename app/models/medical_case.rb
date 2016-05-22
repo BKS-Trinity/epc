d728148470ac752abed9cbc3001c3ff7eac88162
mkozina <mkozina@sigma.ug.edu.pl>
2016-05-22 11:23:29 +0200

    usuwanie powiązanych obiektów w wizytach

--- app/models/medical_case.rb
+++ app/models/medical_case.rb
@@ -2,6 +2,6 @@ class MedicalCase < ActiveRecord::Base
   belongs_to :medical_record
   belongs_to :disease
   belongs_to :doctor
-	has_many :appointments
+	has_many :appointments, dependent: :destroy
 	validates :doctor_id, numericality: { only_integer: true, greater_than: 0 }
 end
