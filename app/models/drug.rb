ca245681eea24b213750dde5e96a13290c4d2dac
mkozina <mkozina@sigma.ug.edu.pl>
2016-06-18 12:32:52 +0200

    dodanie walidacji lekarstw

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,4 +1,8 @@
 class Drug < ActiveRecord::Base
 	has_many :prescriptiondrugs
 	has_many :prescriptions, through: :prescriptiondrugs
+	validates :name, presence: true,
+	length: { in: 3..50 }
+	validates :atc, presence: true,
+	length: { in: 7..8 }
 end
